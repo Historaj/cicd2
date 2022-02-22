9b53c9e6740a6bb488a85716ea1ef6a9f9d76310
Historaj <97240227+Historaj@users.noreply.github.com>
2022-02-22 01:09:03 -0500

    added show api

---
+++ src/main/java/com/cicd2/Cicd2Controller.java
@@ -0,0 +1,18 @@
+package com.cicd2;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class Cicd2Controller {
+	
+	@GetMapping("show/{name}")
+	public ResponseEntity<Object> show(@PathVariable String name) {
+		
+		String msg = "Hello, " + name;
+		return new ResponseEntity<Object>(msg, HttpStatus.OK); 
+	}
+}
